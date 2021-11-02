c5406f261c9375550e22cc378bc86247c122fc1c
Imran95942 <89960334+Imran95942@users.noreply.github.com>
2021-11-02 14:38:16 +0300

    Update config.py

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ HEADERS = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6)"
            "Safari/537.36"}
 
 ##
-token = '<token>'
-token2 = '<token>'
+token = '1989950752:AAHWGMADJGobV_SqpO-wVhrUwurw186LMLA'
+token2 = '1958099910:AAGRUclYR6fsA-DPP10t_BOg62OMyby8hOI'
 
 bot = telebot.TeleBot(token, threaded=True)
 
@@ -23,13 +23,13 @@ bot = telebot.TeleBot(token, threaded=True)
 # бот становится медленней)
 
 s = requests.session()
-heroku_conn = heroku3.from_key('<token heroku>')
-app = heroku_conn.apps()['repo']
+heroku_conn = heroku3.from_key('401e316f-337d-4680-b392-4da5717716b7')
+app = heroku_conn.apps()['isllllm']
 version = len(app.releases()) / 100
 
-chat_id = "<id>"
-user_id = "<id>"
-group_id = "<name_group>"
+chat_id = "-1001488122505"
+user_id = "1901738017"
+group_id = "Hhhhh"
 
 conn = psycopg2.connect(dbname='<dbname>', user='<user>',
                         password='<password>', host='<host>')
